91d7b882f2aabb6d71d92cc472d49f2ec63c4987
Tokyodays <kikuchi@gmail.com>
2020-04-07 09:40:08 +0900

    うぃｐ

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,23 @@
 <template>
   <div>
     <ul v-for="(post, i) in posts" :key="i">
-      <li>{{ post.fields.title }}</li>
+      <li>{{ post.fields.headerImage }}</li>
+      <!-- <li>{{ post.fields.title }}</li>
       <ul>
+        <li>{{post.fields.headerImage}}</li>
         <img
-          :src="post.fields.image.fields.file.url"
-          :alt="post.fields.image.fields.title"
+          :src="post.fields.headerImage.fields.file.url"
+          :alt="post.fields.headerImage.fields.title"
           width="400"/>
         <li>{{ post.fields.body }}</li>
-        <li>{{ post.fields.publishDate }}</li>
-      </ul>
+        <li>{{ post.fields.publishedAt }}</li>
+      </ul> -->
     </ul>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import client from '~/plugins/contentful'
 
 export default {
@@ -22,7 +25,7 @@ export default {
     let posts = []
     await client.getEntries({
       content_type: env.CTF_BLOG_POST_TYPE_ID,
-      order: '-fields.publishDate'
+      order: '-fields.publishedAt'
     }).then(res => (posts = res.items)).catch(console.error)
     return { posts }
   }
---
+++ store/index.js
@@ -0,0 +1,47 @@
+import defaultEyeCatch from '~/assets/images/defaultImage.png'
+// const {getConfigForKeys} = require('@/lib/config.js')
+// const ctfConfig = getConfigForKeys([
+//   'CTF_BLOG_POST_TYPE_ID',
+//   'CTF_SPACE_ID',
+//   'CTF_CDA_ACCESS_TOKEN',
+//   'host'
+// ])
+// const {contentfulCreateClient} = require('@/plugins/contentful')
+// const client = contentfulCreateClient(ctfConfig)
+
+// 追記
+export const state = () => ({
+  posts: []
+})
+
+export const getters = {
+  setEyeCatch: () => (image) => {
+    if (!!image && !!image.fields) return { url: `https:${image.fields.file.url}`, title: image.fields.title }
+    else return { url: defaultEyeCatch, title: 'defaultImage' }
+  },
+  linkTo: () => (name, obj) => {
+    return { name: `${name}-slug`, params: { slug: obj.fields.slug } }
+  }
+}
+
+
+// // 追記
+// export const mutations = {
+//   setPosts(state, payload) {
+//     state.posts = payload
+//   }
+
+// }
+
+// // 追記
+// export const actions = {
+//     async getPosts({ commit }) {
+//         return await client.getEntries({
+//           content_type: process.env.CTF_BLOG_POST_TYPE_ID,
+//             order: '-fields.publishedAt',
+//         }).then(entries => {
+//             commit('setPosts', entries.items)
+//         })
+//         .catch(console.error)
+//     }
+// }
