723bbae160022c6850f4aaca56873439a2ed511f
Tokyodays <kikuchi@gmail.com>
2020-04-10 16:33:45 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,32 @@
 <template>
-  <div>
-    <ul v-for="(post, i) in posts" :key="i">
-      <li>{{ post.fields.title }} <airplay-icon size="1.5x" class="custom-class"></airplay-icon></li>
-      <ul>
-        <img
-          :src="setEyeCatch(post.fields.headerImage).url"
-          :alt="setEyeCatch(post.fields.headerImage).title"
-          width="400"/>
-          <span :is="draftChip(post)" />
-          <p :style="categoryColor(post.fields.category.fields.color)"><nuxt-link :to="linkTo('categories', post.fields.category)">{{ post.fields.category.fields.name }}</nuxt-link></p>
-        <li>{{ post.fields.body }}</li>
-        <li>{{ post.fields.publishedAt }}</li>
-        <li><nuxt-link :to="linkTo('posts', post)">link</nuxt-link></li>
+  <div class="grid grid-cols-4">
+    <header class="col-span-1">aaaa</header>
+    <main class="col-span-3">
+      <ul v-for="(post, i) in posts" :key="i">
+        <li>{{ post.fields.title }} <airplay-icon size="1.5x" class="custom-class"></airplay-icon></li>
+        <ul>
+          <img
+            :src="setEyeCatch(post.fields.headerImage).url"
+            :alt="setEyeCatch(post.fields.headerImage).title"
+            width="400"/>
+            <span :is="draftChip(post)" />
+            <p :style="categoryColor(post.fields.category.fields.color)"><nuxt-link :to="linkTo('categories', post.fields.category)">{{ post.fields.category.fields.name }}</nuxt-link></p>
+          <li>{{ post.fields.publishedAt }}</li>
+          <li><nuxt-link :to="linkTo('posts', post)">link</nuxt-link></li>
+        </ul>
+        <template v-if="post.fields.tags">
+          <div
+            v-for="(tag) in post.fields.tags"
+            :key="tag.sys.id"
+          >
+          <nuxt-link :to="linkTo('tags', tag)">{{ tag.fields.name }}</nuxt-link>
+          </div>
+        </template>
       </ul>
-      <template v-if="post.fields.tags">
-        <div
-          v-for="(tag) in post.fields.tags"
-          :key="tag.sys.id"
-        >
-        <nuxt-link :to="linkTo('tags', tag)">{{ tag.fields.name }}</nuxt-link>
-        </div>
-      </template>
-    </ul>
+
+
+    </main>
+    
   </div>
 </template>
 
